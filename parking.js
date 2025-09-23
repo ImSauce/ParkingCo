fed8d66ebe955d2b99468fdf358606a809f6cf16
Im Sauce <130982799+ImSauce@users.noreply.github.com>
2025-09-23 13:21:33 +0800

    Initial commit

---
+++ parking.js
@@ -0,0 +1,7 @@
+document.getElementById("reserveBtn").addEventListener("click", function () {
+  document.getElementById("slots").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("readMoreBtn").addEventListener("click", function () {
+  document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+});
